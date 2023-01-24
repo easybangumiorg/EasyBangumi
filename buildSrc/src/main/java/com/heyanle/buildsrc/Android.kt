a86a7c9101a42cbb219dc775b6da779b5dbb469b
heyanle <eke_le@163.com>
2023-01-24 23:44:38 +0800

    [feature] 1. 3.0.2 更新

--- buildSrc/src/main/java/com/heyanle/buildsrc/Android.kt
+++ buildSrc/src/main/java/com/heyanle/buildsrc/Android.kt
@@ -9,6 +9,6 @@ object Android{
     val targetSdk = 33
     val compileSdk = 33
 
-    val versionCode = 40
-    val versionName = "3.0.1"
+    val versionCode = 41
+    val versionName = "3.0.2"
 }
