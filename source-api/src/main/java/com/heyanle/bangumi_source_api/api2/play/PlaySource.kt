9b852b8f2daa7796dac65a6bc18e87de601f0112
heyanLe <1371735400@qq.com>
2023-02-20 16:24:27 +0800

    [feature] 1. 源 api 优化

--- source-api/src/main/java/com/heyanle/bangumi_source_api/api2/play/PlaySource.kt
+++ source-api/src/main/java/com/heyanle/bangumi_source_api/api2/play/PlaySource.kt
@@ -10,11 +10,22 @@ import com.heyanle.bangumi_source_api.api2.entity.CartoonSummary
  */
 interface PlaySource: Source {
 
+    /**
+     * 获取番剧详情
+     */
     suspend fun detail(cartoonSummary: CartoonSummary): Source.SourceResult<Cartoon>
 
+    /**
+     * 获取播放线路
+     */
     suspend fun playLine(cartoonSummary: CartoonSummary): Source.SourceResult<List<PlayLine>>
 
-    suspend fun playInfo(cartoonSummary: CartoonSummary, playLine: PlayLine): Source.SourceResult<PlayerInfo>
+    /**
+     * 获取播放信息（播放地址或文件路径）
+     * @param playLine 对应的播放线路
+     * @param episodeIndex 集数
+     */
+    suspend fun playInfo(cartoonSummary: CartoonSummary, playLine: PlayLine, episodeIndex: Int): Source.SourceResult<PlayerInfo>
 
     // 检查番剧更新，如果有更新需要 isUpdate 为 true
     suspend fun update(cartoon: Cartoon): Source.SourceResult<Cartoon>
--- source-api/src/main/java/com/heyanle/bangumi_source_api/api2/search/SearchSource.kt
+++ source-api/src/main/java/com/heyanle/bangumi_source_api/api2/search/SearchSource.kt
@@ -2,7 +2,6 @@ package com.heyanle.bangumi_source_api.api2.search
 
 import com.heyanle.bangumi_source_api.api2.Source
 import com.heyanle.bangumi_source_api.api2.entity.CartoonCover
-import com.heyanle.bangumi_source_api.api2.home.HomePage
 
 /**
  * Created by HeYanLe on 2023/2/18 21:39.
