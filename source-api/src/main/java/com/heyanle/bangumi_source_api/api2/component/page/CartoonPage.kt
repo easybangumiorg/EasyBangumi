78af0c15f87d0fe740361e009ec8db9efaeeaa5f
heyanLe <1371735400@qq.com>
2023-02-25 15:16:27 +0800

    [feature] 1. 源 api 变继承于组合

--- source-api/src/main/java/com/heyanle/bangumi_source_api/api2/component/page/CartoonPage.kt
+++ source-api/src/main/java/com/heyanle/bangumi_source_api/api2/component/page/CartoonPage.kt
@@ -1,16 +1,16 @@
-package com.heyanle.bangumi_source_api.api2.page
+package com.heyanle.bangumi_source_api.api2.component.page
+
+import com.heyanle.bangumi_source_api.api2.component.Component
+
 
 /**
  * 番剧页抽象
  * Created by HeYanLe on 2023/2/20 16:00.
  * https://github.com/heyanLE
  */
-interface CartoonPage {
+interface CartoonPage : Component {
     // 页面标题
     val label: String
-
-    // 隶属的源 key
-    val sourceKey: String
 
     // 是否启动新界面展示
     val newScreen: Boolean
