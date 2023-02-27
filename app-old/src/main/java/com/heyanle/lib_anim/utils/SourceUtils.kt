0f6d1f08fa9a6e7c3655c07e4f920349969c5234
heyanle <eke_le@163.com>
2023-02-28 00:34:03 +0800

    [feature] 1.app old

---
+++ app-old/src/main/java/com/heyanle/lib_anim/utils/SourceUtils.kt
@@ -0,0 +1,23 @@
+package com.heyanle.lib_anim.utils
+
+object SourceUtils {
+    fun urlParser(rootURL: String, source: String): String {
+        return when {
+            source.startsWith("http") -> {
+                source
+            }
+
+            source.startsWith("//") -> {
+                "https:$source"
+            }
+
+            source.startsWith("/") -> {
+                rootURL + source
+            }
+
+            else -> {
+                "${rootURL}/$source"
+            }
+        }
+    }
+}
