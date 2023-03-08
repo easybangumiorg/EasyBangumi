59ad3bf6c1135e787bec577af57b4b1087f259ae
heyanLe <1371735400@qq.com>
2023-03-08 23:19:10 +0800

    [feature] 1.详情播放页 60% 2.简单写了下 Compose 播放器状态机的状态和接口（解构得脑壳疼），明天组合实现

--- app/src/main/java/com/heyanle/easybangumi4/ui/cartoon_play/CartoonPlay.kt
+++ app/src/main/java/com/heyanle/easybangumi4/ui/cartoon_play/CartoonPlay.kt
@@ -12,21 +12,36 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CastConnected
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.StarOutline
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.heyanle.bangumi_source_api.api.Source
@@ -34,7 +49,11 @@ import com.heyanle.bangumi_source_api.api.entity.Cartoon
 import com.heyanle.bangumi_source_api.api.entity.CartoonSummary
 import com.heyanle.bangumi_source_api.api.entity.PlayLine
 import com.heyanle.easybangumi4.LocalNavController
+import com.heyanle.easybangumi4.R
+import com.heyanle.easybangumi4.ui.common.Action
+import com.heyanle.easybangumi4.ui.common.ActionRow
 import com.heyanle.easybangumi4.ui.common.DetailedContainer
+import com.heyanle.easybangumi4.ui.common.EmptyPage
 import com.heyanle.easybangumi4.ui.common.OkImage
 
 /**
@@ -52,7 +71,6 @@ fun CartoonPlay(
 
     val summary = CartoonSummary(id, source, url)
     val owner = DetailedViewModel.getViewModelStoreOwner(summary)
-
     CompositionLocalProvider(
         LocalViewModelStoreOwner provides owner
     ) {
@@ -98,6 +116,8 @@ fun CartoonPlay(
     }
 
 
+
+
     Surface(
         color = MaterialTheme.colorScheme.background,
         contentColor = MaterialTheme.colorScheme.onBackground
@@ -108,6 +128,18 @@ fun CartoonPlay(
 
 }
 
+
+@Composable
+fun CartoonPlayUI(
+    detailedVM: DetailedViewModel,
+    cartoonPlayVM: CartoonPlayViewModel,
+    isFullScreen: Boolean,
+){
+
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CartoonPlayDetailed(
     modifier: Modifier,
@@ -116,8 +148,33 @@ fun CartoonPlayDetailed(
     selectLineIndex: Int,
     playingPlayLine: PlayLine,
     playingEpisode: Int,
-    onClick: (Int, PlayLine, Int) -> Unit
+    onLineSelect: (Int) -> Unit,
+    onEpisodeClick: (Int, PlayLine, Int) -> Unit,
+
+    isStar: Boolean,
+    onStar: (Boolean)->Unit,
+    onSearch: ()->Unit,
+    onWeb: ()->Unit,
+    onDlna: ()->Unit,
 ) {
+
+    // 将非空的 播放线路 下标存成离散序列
+    val unEmptyLinesIndex = remember(playLines) {
+        arrayListOf<Int>().apply{
+            playLines.forEachIndexed { index, playLine ->
+                if(playLine.episode.isNotEmpty()){
+                    add(index)
+                }
+            }
+        }
+    }
+
+    LaunchedEffect(key1 = playLines, key2 = selectLineIndex){
+        if(!unEmptyLinesIndex.contains(selectLineIndex) && unEmptyLinesIndex.isNotEmpty()){
+            onLineSelect(unEmptyLinesIndex[0])
+        }
+    }
+
 
 
     Column(modifier = modifier) {
@@ -150,15 +207,67 @@ fun CartoonPlayDetailed(
                     GridItemSpan(maxLineSpan)
                 }
             ) {
-
+                CartoonActions(
+                    isStar = isStar,
+                    onStar = onStar,
+                    onSearch = onSearch,
+                    onWeb = onWeb,
+                    onDlna = onDlna
+                )
             }
 
+            if(unEmptyLinesIndex.isNotEmpty()){
+                item (
+                    span = {
+                        // LazyGridItemSpanScope:
+                        // maxLineSpan
+                        GridItemSpan(maxLineSpan)
+                    }
+                ){
+                    EmptyPage(
+                        modifier = Modifier.fillMaxWidth(),
+                        emptyMsg = stringResource(id = com.heyanle.easy_i18n.R.string.no_play_line)
+                    )
+                }
+            }else{
+                item (
+                    span = {
+                        // LazyGridItemSpanScope:
+                        // maxLineSpan
+                        GridItemSpan(maxLineSpan)
+                    }
+                ) {
+                    ScrollableTabRow(selectedTabIndex = unEmptyLinesIndex.indexOf(selectLineIndex)) {
+                        unEmptyLinesIndex.forEach {index ->
+                            val playLine = playLines[index]
+                            Tab(
+                                selected = index == selectLineIndex,
+                                onClick = {
+                                    onLineSelect(index)
+                                },
+                                text = {
+                                    Text(text = playLine.label)
+                                }
+                            )
+                        }
+                    }
+                }
 
+                if(selectLineIndex >= 0 && selectLineIndex < playLines.size && unEmptyLinesIndex.contains(selectLineIndex)){
+                    itemsIndexed(playLines[selectLineIndex].episode){ index, item ->
+                        FilterChip(
+                            selected = playLines[selectLineIndex] == playingPlayLine && index == playingEpisode,
+                            onClick = {
+                                onEpisodeClick(selectLineIndex, playLines[selectLineIndex], index)
+                            },
+                            label = { Text(item) },
+                            colors = FilterChipDefaults.filterChipColors(),
+                        )
+                    }
+                }
+            }
         }
-
-
     }
-
 }
 
 
@@ -191,6 +300,99 @@ fun CartoonDescCard(
         )
     }
 }
+
+@Composable
+fun CartoonActions(
+    isStar: Boolean,
+    onStar: (Boolean)->Unit,
+    onSearch: ()->Unit,
+    onWeb: ()->Unit,
+    onDlna: ()->Unit,
+){
+    ActionRow (
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        val starIcon =
+            if (isStar) Icons.Filled.Star else Icons.Filled.StarOutline
+        val starTextId =
+            if (isStar) com.heyanle.easy_i18n.R.string.stared else com.heyanle.easy_i18n.R.string.click_star
+        // 点击追番
+        Action(
+            icon = {
+                Icon(
+                    starIcon,
+                    stringResource(id = starTextId),
+                    tint = if (isStar) MaterialTheme.colorScheme.secondary else MaterialTheme.colorScheme.onBackground
+                )
+            },
+            msg = {
+                Text(
+                    text = stringResource(id = starTextId),
+                    color = if (isStar) MaterialTheme.colorScheme.secondary else MaterialTheme.colorScheme.onBackground,
+                    fontSize = 12.sp
+                )
+            },
+            onClick = {
+                onStar(! isStar)
+            }
+        )
+
+        // 搜索同名番
+        Action(
+            icon = {
+                Icon(
+                    Icons.Filled.Search,
+                    stringResource(id = com.heyanle.easy_i18n.R.string.search_same_bangumi)
+                )
+            },
+            msg = {
+                Text(
+                    text = stringResource(id = com.heyanle.easy_i18n.R.string.search_same_bangumi),
+                    fontSize = 12.sp
+                )
+            },
+            onClick = onSearch
+        )
+
+        // 打开原网站
+        Action(
+            icon = {
+                Icon(
+                    painterResource(id = R.drawable.ic_webview_24dp),
+                    stringResource(id = com.heyanle.easy_i18n.R.string.open_source_url)
+                )
+            },
+            msg = {
+                Text(
+                    text = stringResource(id = com.heyanle.easy_i18n.R.string.open_source_url),
+                    fontSize = 12.sp
+                )
+            },
+            onClick = onWeb
+        )
+
+        // 投屏
+        Action(
+            icon = {
+                Icon(
+                    Icons.Filled.CastConnected,
+                    stringResource(id = com.heyanle.easy_i18n.R.string.screen_cast)
+                )
+            },
+            msg = {
+                Text(
+                    text = stringResource(id = com.heyanle.easy_i18n.R.string.screen_cast),
+                    fontSize = 12.sp
+                )
+            },
+            onClick = onDlna
+        )
+    }
+
+
+}
+
+
 
 
 
---
+++ app/src/main/java/com/heyanle/easybangumi4/ui/common/player/utils/TimeUtils.kt
@@ -0,0 +1,13 @@
+package com.heyanle.easybangumi4.ui.common.player.utils
+
+object TimeUtils {
+
+    fun toString(ti: Long): String {
+        val time = (ti / 1000).toInt()
+        val min = (time / 60).toInt()
+        val se = (time % 60).toInt()
+
+        return "${if (min >= 10) min else "0$min"}:${if (se >= 10) se else "0$se"}"
+    }
+
+}
