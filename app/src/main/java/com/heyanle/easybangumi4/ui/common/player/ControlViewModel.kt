5c9d6e9b739d02a00abd3323ff8806a036b6260b
heyanle <eke_le@163.com>
2023-03-09 12:25:55 +0800

    [feature] 1.logo 补提交

--- app/src/main/java/com/heyanle/easybangumi4/ui/common/player/ControlViewModel.kt
+++ app/src/main/java/com/heyanle/easybangumi4/ui/common/player/ControlViewModel.kt
@@ -1,10 +1,17 @@
 package com.heyanle.easybangumi4.ui.common.player
 
+import androidx.annotation.UiThread
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.Player
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 /**
  * Created by HeYanLe on 2023/3/8 22:45.
@@ -12,12 +19,12 @@ import com.google.android.exoplayer2.ExoPlayer
  */
 class ControlViewModel(
     private val exoPlayer: ExoPlayer
-): ViewModel() {
+): ViewModel(), Player.Listener {
 
-    // 状态 长按加速中 锁定中 左右滑动中 上下滑动中 普通
+    // 普通状态  长按加速中 锁定中 左右滑动中 上下滑动中 结束
 
     enum class ControlState {
-        Normal, Locked, HorizontalScroll, VerticalDrag, LongTouched
+        Normal, Locked, HorizontalScroll, VerticalDrag, LongTouched, Ended
     }
 
     var controlState by mutableStateOf(ControlState.Normal)
@@ -40,26 +47,142 @@ class ControlViewModel(
 
     var isLoading by mutableStateOf(false)
 
+    var playWhenReady by mutableStateOf(exoPlayer.playWhenReady)
+
     var position by mutableStateOf(0L)
     var bufferPosition by mutableStateOf(0L)
 
     var during by mutableStateOf(0L)
 
+    private var loopJob: Job? = null
 
 
-    fun onLockedChange(locked: Boolean){}
 
-    fun onFullScreen(fullScreen: Boolean){}
+    fun onLockedChange(locked: Boolean){
+        viewModelScope.launch {
+            if(locked){
+                controlState = ControlState.Locked
+                isLockedShow = true
+            }else{
+                controlState = ControlState.Normal
+                isNormalShow = true
+            }
+        }
+    }
 
-    fun onPlayPause(isPlay: Boolean){}
+    fun onFullScreen(fullScreen: Boolean){
+        viewModelScope.launch {
+            if(isFullScreen == fullScreen){
+                return@launch
+            }
+            isFullScreen = fullScreen
+            controlState = ControlState.Normal
+            isNormalShow = true
+        }
+    }
 
-    fun onPrepare(){}
+    @UiThread
+    fun onPlayPause(isPlay: Boolean){
+        exoPlayer.playWhenReady = isPlay
+    }
 
-    fun onPositionChange(position: Long){}
+    @UiThread
+    fun onPrepare(){
+        isLoading = true
+    }
 
-    fun onPositionChangeFinished(){}
+    @UiThread
+    fun onPositionChange(position: Long){
+        horizontalScrollPosition = position
+        if(controlState != ControlState.HorizontalScroll){
+            controlState = ControlState.HorizontalScroll
+        }
+
+    }
+
+    fun onPositionChangeFinished(){
+        controlState = ControlState.Normal
+        isNormalShow = true
+        exoPlayer.seekTo(horizontalScrollPosition)
+    }
 
     fun onHideClick(){}
 
+    init {
+        exoPlayer.addListener(this)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        exoPlayer.removeListener(this)
+    }
+
+    private fun getLoopJob(): Job {
+        return viewModelScope.launch {
+            while (isActive){
+                syncTimeIfNeed()
+                delay(1000)
+            }
+        }
+    }
+
+    private fun starLoop(){
+        viewModelScope.launch {
+            if(loopJob == null || loopJob?.isActive != true){
+                loopJob?.cancel()
+                loopJob = getLoopJob()
+            }
+        }
+    }
+
+    private fun stopLoop(){
+        viewModelScope.launch {
+            loopJob?.cancel()
+            loopJob = null
+        }
+    }
+
+
+
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        super.onPlaybackStateChanged(playbackState)
+        when(playbackState){
+            Player.STATE_READY -> {
+                isLoading = true
+                syncTimeIfNeed()
+                starLoop()
+            }
+            Player.STATE_IDLE -> {
+                isLoading = false
+                stopLoop()
+            }
+            Player.STATE_BUFFERING -> {
+                isLoading = true
+                syncTimeIfNeed()
+                starLoop()
+            }
+            Player.STATE_ENDED -> {
+                isLoading = false
+                stopLoop()
+            }
+        }
+    }
+
+    override fun onPlayWhenReadyChanged(playWhenReady: Boolean, reason: Int) {
+        super.onPlayWhenReadyChanged(playWhenReady, reason)
+        this.playWhenReady = playWhenReady
+    }
+
+    private fun syncTimeIfNeed(){
+        if(during != exoPlayer.duration){
+            during = exoPlayer.duration
+        }
+        if(position != exoPlayer.currentPosition){
+            position = exoPlayer.currentPosition
+        }
+        if(bufferPosition != exoPlayer.bufferedPosition){
+            bufferPosition = exoPlayer.bufferedPosition
+        }
+    }
 
 }
