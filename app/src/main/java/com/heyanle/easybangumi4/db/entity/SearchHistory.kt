a3bacba29e12699b77a555c1071764bcb351f6e3
heyanle <eke_le@163.com>
2023-03-29 22:38:15 +0800

    [feature]1. 聚合搜索界面

---
+++ app/src/main/java/com/heyanle/easybangumi4/db/entity/SearchHistory.kt
@@ -0,0 +1,12 @@
+package com.heyanle.easybangumi4.db.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class SearchHistory(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val timestamp: Long,
+    val content: String
+)
