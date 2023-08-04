18c83f55fc56c2bb0acb5a27b173e72618c4c51a
heyanle <eke_le@163.com>
2023-08-04 17:06:56 +0800

    [feature] 1.cartoon cover 收藏逻辑抽取

--- app/src/main/java/com/heyanle/easybangumi4/compose/common/page/list/SourceListViewModel.kt
+++ app/src/main/java/com/heyanle/easybangumi4/compose/common/page/list/SourceListViewModel.kt
@@ -1,5 +1,6 @@
 package com.heyanle.easybangumi4.compose.common.page.list
 
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -10,7 +11,7 @@ import androidx.paging.cachedIn
 import com.heyanle.bangumi_source_api.api.component.page.SourcePage
 import com.heyanle.bangumi_source_api.api.entity.CartoonCover
 import com.heyanle.bangumi_source_api.api.entity.CartoonSummary
-import com.heyanle.easybangumi4.R
+import com.heyanle.bangumi_source_api.api.entity.toIdentify
 import com.heyanle.easybangumi4.base.db.dao.CartoonStarDao
 import com.heyanle.easybangumi4.base.entity.CartoonStar
 import com.heyanle.easybangumi4.compose.common.moeSnackBar
@@ -18,14 +19,11 @@ import com.heyanle.easybangumi4.compose.common.page.paging.ListPagePagingSource
 import com.heyanle.easybangumi4.source.SourceLibraryController
 import com.heyanle.easybangumi4.utils.stringRes
 import com.heyanle.injekt.core.Injekt
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import java.util.concurrent.ConcurrentMap
 
 /**
  * Created by HeYanLe on 2023/2/25 20:45.
@@ -37,18 +35,7 @@ class SourceListViewModel(
 
     val curPager = mutableStateOf(getPager().flow.cachedIn(viewModelScope))
 
-    private val cartoonStarDao: CartoonStarDao by Injekt.injectLazy()
-    val starFlow =
-        cartoonStarDao.flowAll()
-            .map {
-                it.map {
-                    it.getIdentify()
-                }.toSet()
-            }.stateIn(viewModelScope, SharingStarted.Lazily, emptySet())
 
-    val curStar = mutableStateOf<CartoonCover?>(null)
-
-    private val sourceController: SourceLibraryController by Injekt.injectLazy()
 
     fun refresh() {
         curPager.value = getPager().flow.cachedIn(viewModelScope)
@@ -61,45 +48,6 @@ class SourceListViewModel(
         ) {
             ListPagePagingSource(listPage)
         }
-    }
-
-    private var lastStarJob: Job? = null
-    fun longPress(cartoonCover: CartoonCover) {
-        lastStarJob?.cancel()
-        lastStarJob = viewModelScope.launch {
-            val isStar = starFlow.value.contains("${cartoonCover.id} ${cartoonCover.source} ${cartoonCover.url}")
-            if(isStar || isCoverCur(cartoonCover)){
-                curStar.value = null
-                lastStarJob?.cancel()
-                cartoonStarDao.deleteByCartoonSummary(cartoonCover.id, cartoonCover.source, cartoonCover.url)
-            }else{
-                curStar.value = cartoonCover
-                sourceController.sourceBundleFlow.value.detailed(cartoonCover.source)?.getAll(
-                    CartoonSummary(cartoonCover.id, cartoonCover.source, cartoonCover.url)
-                )?.complete {
-                    if(isActive){
-                        cartoonStarDao.modify(CartoonStar.fromCartoon(it.data.first, it.data.second).apply {
-                            reversal = false
-                        })
-                        curStar.value = null
-                    }
-
-                }?.error {
-                    it.throwable.printStackTrace()
-                    if(isActive) {
-                        curStar.value = null
-                        (stringRes(com.heyanle.easy_i18n.R.string.detailed_error) + it.throwable.message).moeSnackBar()
-                    }
-                }
-            }
-        }
-
-
-
-    }
-
-    fun isCoverCur(cartoonCover: CartoonCover): Boolean{
-        return curStar.value?.id == cartoonCover.id && curStar.value?.source == cartoonCover.source && curStar.value?.url == cartoonCover.url
     }
 
 
---
+++ app/src/main/java/com/heyanle/easybangumi4/compose/main/star/CoverStarViewModel.kt
@@ -0,0 +1,82 @@
+package com.heyanle.easybangumi4.compose.main.star
+
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.heyanle.bangumi_source_api.api.entity.CartoonCover
+import com.heyanle.bangumi_source_api.api.entity.CartoonSummary
+import com.heyanle.bangumi_source_api.api.entity.toIdentify
+import com.heyanle.easy_i18n.R
+import com.heyanle.easybangumi4.base.db.dao.CartoonStarDao
+import com.heyanle.easybangumi4.base.entity.CartoonStar
+import com.heyanle.easybangumi4.compose.common.moeSnackBar
+import com.heyanle.easybangumi4.source.SourceLibraryController
+import com.heyanle.easybangumi4.utils.stringRes
+import com.heyanle.injekt.core.Injekt
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+
+/**
+ * CartoonCover 的 star 逻辑抽取
+ * Created by heyanlin on 2023/8/4.
+ * https://github.com/heyanLE
+ */
+class CoverStarViewModel : ViewModel() {
+
+    private val cartoonStarDao: CartoonStarDao by Injekt.injectLazy()
+    val starFlow =
+        cartoonStarDao.flowAll()
+            .map { stars ->
+                stars.map {
+                    it.toIdentify()
+                }.toSet()
+            }.stateIn(viewModelScope, SharingStarted.Lazily, emptySet())
+
+    // 因为收藏番剧需要拉取番剧所有数据，有耗时，因此这里触发收藏后先临时展示收藏完成
+    // 当开始收藏任务后先加到该临时列表，收藏失败或者成功都移除
+    // 该列表中的番剧在展示上视为已收藏
+    private val staringCartoon = mutableStateMapOf<String, Boolean>()
+
+    private val sourceController: SourceLibraryController by Injekt.injectLazy()
+
+    fun star(cartoonCover: CartoonCover) {
+        viewModelScope.launch {
+            val identify = cartoonCover.toIdentify()
+            val isStar = starFlow.value.contains(identify)
+            if (isStar || staringCartoon.contains(identify)) {
+                // 临时列表和数据库都删了
+                // 因为是主线程不用考虑并发问题
+                staringCartoon.remove(identify)
+                cartoonStarDao.deleteByCartoonSummary(
+                    cartoonCover.id,
+                    cartoonCover.source,
+                    cartoonCover.url
+                )
+            } else {
+                staringCartoon[identify] = true
+                sourceController.sourceBundleFlow.value.detailed(cartoonCover.source)?.getAll(
+                    CartoonSummary(cartoonCover.id, cartoonCover.source, cartoonCover.url)
+                )?.complete {
+                    cartoonStarDao.modify(
+                        CartoonStar.fromCartoon(it.data.first, it.data.second).apply {
+                            reversal = false
+                        })
+                    staringCartoon.remove(identify)
+                }?.error {
+                    it.throwable.printStackTrace()
+                    if (isActive) {
+                        staringCartoon.remove(identify)
+                        (stringRes(R.string.detailed_error) + it.throwable.message).moeSnackBar()
+                    }
+                }
+            }
+        }
+    }
+
+    fun isCoverStaring(cartoonCover: CartoonCover): Boolean {
+        return staringCartoon[cartoonCover.toIdentify()] == true
+    }
+}
