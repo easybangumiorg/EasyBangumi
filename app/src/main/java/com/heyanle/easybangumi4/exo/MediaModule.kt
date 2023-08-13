c81d9998b5c88a39b1355a2e8c48d7c6713b2774
heyanLe <1371735400@qq.com>
2023-08-13 20:30:32 +0800

    [feature]1.优化整体缓存逻辑，为下载铺路

--- app/src/main/java/com/heyanle/easybangumi4/exo/MediaModule.kt
+++ app/src/main/java/com/heyanle/easybangumi4/exo/MediaModule.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.media3.database.DatabaseProvider
 import androidx.media3.database.StandaloneDatabaseProvider
 import androidx.media3.datasource.DefaultHttpDataSource
+import androidx.media3.datasource.HttpDataSource
 import androidx.media3.datasource.cache.Cache
 import androidx.media3.datasource.cache.CacheDataSink
 import androidx.media3.datasource.cache.CacheDataSource
@@ -16,6 +17,8 @@ import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
 import com.heyanle.easybangumi4.preferences.SettingPreferences
 import com.heyanle.easybangumi4.utils.getCachePath
 import com.heyanle.easybangumi4.utils.getFilePath
+import com.heyanle.easybangumi4.utils.loge
+import com.heyanle.easybangumi4.utils.mb
 import com.heyanle.injekt.api.InjektModule
 import com.heyanle.injekt.api.InjektScope
 import com.heyanle.injekt.api.addPerKeyFactory
@@ -42,8 +45,13 @@ class MediaModule(
         }
 
         addSingletonFactory {
+            StandaloneDatabaseProvider(application)
+        }
+
+        addSingletonFactory {
             val settingPreferences: SettingPreferences = get()
             val cacheSize = settingPreferences.cacheSizeMB.get()
+            cacheSize.loge("MediaModule")
             val dataSourceFactory = get<CacheDataSource.Factory>(cacheSize)
             val mediaSourceFactory =
                 DefaultMediaSourceFactory(application).setDataSourceFactory(dataSourceFactory)
@@ -53,19 +61,23 @@ class MediaModule(
                 .build()
         }
 
+        addSingletonFactory<HttpDataSource.Factory> {
+            DefaultHttpDataSource.Factory()
+        }
+
         // 以下实体都跟缓存上限有关，0 为无限制或下载
 
         // Cache
         addPerKeyFactory<Cache, Long> { cacheSize ->
             if (cacheSize == 0L) { // 缓存无上限直接缓存到下载地址
                 val downloadFolder = File(application.getFilePath(), "download")
-                SimpleCache(downloadFolder, NoOpCacheEvictor(), get())
+                SimpleCache(downloadFolder, NoOpCacheEvictor(), get<StandaloneDatabaseProvider>())
             } else {
-                val lruEvictor = LeastRecentlyUsedCacheEvictor(cacheSize)
+                val lruEvictor = LeastRecentlyUsedCacheEvictor(cacheSize.mb)
                 SimpleCache(
                     File(application.getCachePath("media")),
                     lruEvictor,
-                    MediaCacheDB(application)
+                    get<MediaCacheDB>()
                 )
             }
         }
@@ -76,6 +88,7 @@ class MediaModule(
             if (cacheSize == 0L) {
                 CacheDataSource.Factory()
                     .setCache(cache)
+                    .setUpstreamDataSourceFactory(get<HttpDataSource.Factory>())
                     .setCacheWriteDataSinkFactory(null) // 只读不写，只允许下载器写
                     .setFlags(CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR)
             } else {
@@ -99,9 +112,9 @@ class MediaModule(
         addSingletonFactory {
             DownloadManager(
                 application,
-                get<MediaCacheDB>(),
+                get<StandaloneDatabaseProvider>(),
                 get<Cache>(0),
-                DefaultHttpDataSource.Factory(),
+                get<HttpDataSource.Factory>(),
                 Executors.newFixedThreadPool(6)
             )
         }
