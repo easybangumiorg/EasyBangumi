e93dd2933af9c693e5d6730833651b3e2caea553
heyanlin <heyanlin@tencent.com>
2023-08-23 19:56:48 +0800

    [feature] 1.迁移到 version catelog 2.升级md3 30%

---
+++ okm3u8/okm3u8-core/src/main/java/com/heyanle/okm3u8/Download.kt
@@ -0,0 +1,12 @@
+package com.heyanle.okm3u8
+
+/**
+ * Created by heyanlin on 2023/8/16.
+ */
+suspend fun download(
+    url: String,
+    header: Map<String, String>,
+    target: String,
+){
+
+}
