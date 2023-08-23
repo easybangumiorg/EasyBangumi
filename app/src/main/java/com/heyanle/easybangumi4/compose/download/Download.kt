b73fbf38ca311dc47278d3d01f00b3b32cbeff10
heyanle <eke_le@163.com>
2023-08-23 19:56:48 +0800

    [feature] 1.迁移到 version catelog 2.升级md3 30%

---
+++ app/src/main/java/com/heyanle/easybangumi4/compose/download/Download.kt
@@ -0,0 +1,10 @@
+package com.heyanle.easybangumi4.compose.download
+
+import androidx.compose.runtime.Composable
+
+/**
+ * Created by heyanlin on 2023/8/9.
+ * https://github.com/heyanLE
+ */
+@Composable
+fun Download(){}
---
+++ app/src/main/java/com/heyanle/easybangumi4/download/DownloadService.kt
@@ -0,0 +1,8 @@
+package com.heyanle.easybangumi4.download
+
+/**
+ * Created by heyanlin on 2023/8/9.
+ * https://github.com/heyanLE
+ */
+class DownloadService {
+}
