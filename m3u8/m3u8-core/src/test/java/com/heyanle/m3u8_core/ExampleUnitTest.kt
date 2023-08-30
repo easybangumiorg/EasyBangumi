f1f102691ab208e32e4dd0def87d91add2210f1e
heyanlin <heyanlin@tencent.com>
2023-08-30 19:33:47 +0800

    [feature]1、下载模块

--- m3u8/m3u8-core/src/test/java/com/heyanle/m3u8_core/ExampleUnitTest.kt
+++ m3u8/m3u8-core/src/test/java/com/heyanle/m3u8_core/ExampleUnitTest.kt
@@ -1,8 +1,6 @@
 package com.heyanle.m3u8_core
 
-import org.junit.Test
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +8,5 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
+
 }
