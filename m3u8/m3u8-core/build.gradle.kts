584282f170dfe889b5e28611c4ee225d24038113
heyanlin <heyanlin@tencent.com>
2023-08-30 19:26:40 +0800

    [feature]1、下载模块

---
+++ m3u8/m3u8-core/build.gradle.kts
@@ -0,0 +1,41 @@
+import com.heyanle.buildsrc.Android
+
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(build.plugins.android.library)
+    alias(build.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.heyanle.m3u8_core"
+    compileSdk = Android.compileSdk
+
+    defaultConfig {
+        minSdk = Android.minSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.okhttp3)
+}
