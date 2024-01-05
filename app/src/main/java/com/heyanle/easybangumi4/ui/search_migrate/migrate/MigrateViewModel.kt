bc73018b7983d70537ae74e197a6230870371ebd
heyanle <eke_le@163.com>
2024-01-05 17:49:47 +0800

    [feature] 1.迁移 50%

--- app/src/main/java/com/heyanle/easybangumi4/ui/search_migrate/migrate/MigrateViewModel.kt
+++ app/src/main/java/com/heyanle/easybangumi4/ui/search_migrate/migrate/MigrateViewModel.kt
@@ -5,14 +5,21 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.heyanle.easybangumi4.base.DataResult
 import com.heyanle.easybangumi4.cartoon.entity.CartoonInfo
+import com.heyanle.easybangumi4.cartoon.repository.db.dao.CartoonInfoDao
 import com.heyanle.easybangumi4.case.CartoonInfoCase
+import com.heyanle.easybangumi4.case.SourceStateCase
+import com.heyanle.easybangumi4.source_api.entity.Cartoon
 import com.heyanle.easybangumi4.source_api.entity.CartoonSummary
+import com.heyanle.easybangumi4.utils.CoroutineProvider
 import com.heyanle.easybangumi4.utils.ViewModelOwnerMap
+import com.heyanle.easybangumi4.utils.toJson
 import com.heyanle.injekt.core.Injekt
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import loli.ball.easyplayer2.utils.systemVolume
 
 /**
  * Created by heyanle on 2023/12/23.
@@ -26,11 +33,17 @@ class MigrateViewModel(
 
     private val ownerMap = ViewModelOwnerMap<CartoonInfo>()
 
+
+    private val cartoonInfoDao: CartoonInfoDao by Injekt.injectLazy()
     private val cartoonInfoCase: CartoonInfoCase by Injekt.injectLazy()
+    private val sourceCase: SourceStateCase by Injekt.injectLazy()
+
+    private val migrateDispatcher = CoroutineProvider.SINGLE
 
 
     data class MigrateState(
         val isLoading: Boolean = true,
+        val isMigrating: Boolean = false,
         val infoList: List<CartoonInfo> = emptyList(),
         val selection: Set<CartoonInfo> = emptySet(),
     )
@@ -51,7 +64,7 @@ class MigrateViewModel(
             _infoListFlow.update {
                 it.copy(
                     false,
-                    infoList
+                    infoList = infoList
                 )
             }
         }
@@ -88,18 +101,18 @@ class MigrateViewModel(
     }
 
     fun selectLongPress(cartoonInfo: CartoonInfo) {
-        if(lastSelectInfo == null){
+        if (lastSelectInfo == null) {
             selectChange(cartoonInfo)
             return
         }
         val currInfo = _infoListFlow.value.infoList
         val lastIndex = currInfo.indexOf(lastSelectInfo)
-        if(lastIndex == -1){
+        if (lastIndex == -1) {
             selectChange(cartoonInfo)
             return
         }
         val nowIndex = currInfo.indexOf(cartoonInfo)
-        if(nowIndex == -1){
+        if (nowIndex == -1) {
             selectChange(cartoonInfo)
             return
         }
@@ -109,7 +122,7 @@ class MigrateViewModel(
         _infoListFlow.update {
             it.copy(
                 selection = it.selection.toMutableSet().apply {
-                    for(i in min..max){
+                    for (i in min..max) {
                         val car = currInfo.getOrNull(i) ?: continue
                         if (contains(car)) {
                             remove(car)
@@ -165,6 +178,70 @@ class MigrateViewModel(
             it.copy(
                 infoList = it.infoList.filter { it != cartoonInfo }
             )
+        }
+    }
+
+    fun migrate(
+        migrate: List<Pair<CartoonInfo, MigrateItemViewModel.MigrateItemState>>,
+        onCompletely: () -> Unit = {}
+    ) {
+
+        viewModelScope.launch(migrateDispatcher) {
+            _infoListFlow.update {
+                it.copy(
+                    isMigrating = true
+                )
+            }
+
+            val bundle = sourceCase.awaitBundle()
+            val actions = arrayListOf<Pair<CartoonInfo, CartoonInfo>>()
+            for (pair in migrate) {
+                val car = pair.second.cartoon ?: continue
+                val sourceName = bundle.source(car.source)?.label ?: ""
+                val episodeList = pair.second.playLineWrapper?.sortedEpisodeList
+                val cartoonInfo = CartoonInfo.fromCartoon(
+                    car,
+                    sourceName,
+                    pair.second.playLineList
+                ).copy(
+                    tags = pair.first.tags,
+                    upTime = pair.first.upTime,
+
+                    starTime = if (pair.first.starTime == 0L) System.currentTimeMillis() else pair.first.starTime,
+                    lastHistoryTime = pair.first.lastHistoryTime,
+                    lastPlayLineEpisodeString = episodeList?.toJson() ?: "",
+                    lastLineId = pair.second.playLine?.id ?: "",
+                    lastLinesIndex = pair.second.playLineList.indexOf(pair.second.playLine) ?: -1,
+                    lastLineLabel = pair.second.playLine?.label ?: "",
+
+                    lastEpisodeLabel = pair.second.playLine?.label ?: "",
+                    lastEpisodeId = pair.second.episode?.id ?: "",
+                    lastEpisodeIndex = episodeList?.indexOf(pair.second.episode) ?: -1,
+                    lastEpisodeOrder = pair.second.episode?.order ?: -1,
+
+                    lastProcessTime = 0,
+                )
+                actions.add(pair.first to cartoonInfo)
+            }
+
+            actions.map {
+                async {
+                    cartoonInfoDao.modify(it.second)
+                    cartoonInfoDao.deleteStar(it.first)
+                }
+            }.forEach {
+                it.await()
+            }
+
+            _infoListFlow.update {
+                it.copy(
+                    isMigrating = false
+                )
+            }
+
+            viewModelScope.launch {
+                onCompletely()
+            }
         }
     }
 
