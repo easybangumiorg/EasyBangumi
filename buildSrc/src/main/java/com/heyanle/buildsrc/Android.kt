8dcd9cdfa5cdcba81dad3fd3618b389684afe93d
heyanle <1371735400@qq.com>
2024-02-24 18:45:05 +0800

    [release] 5.1.2

--- buildSrc/src/main/java/com/heyanle/buildsrc/Android.kt
+++ buildSrc/src/main/java/com/heyanle/buildsrc/Android.kt
@@ -9,8 +9,8 @@ object Android {
     const val targetSdk = 34
     const val compileSdk = 34
 
-    const val versionCode = 83
-    const val versionName = "5.1.1"
+    const val versionCode = 84
+    const val versionName = "5.1.2"
 
 
 }
