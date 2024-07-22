1f6424f798944dc88586e83f5d6ac05a5862e229
heyanle <eke_le@163.com>
2024-07-22 23:06:40 +0800

    [release] 5.4.2

--- buildSrc/src/main/java/com/heyanle/buildsrc/Android.kt
+++ buildSrc/src/main/java/com/heyanle/buildsrc/Android.kt
@@ -9,8 +9,8 @@ object Android {
     const val targetSdk = 34
     const val compileSdk = 34
 
-    const val versionCode = 93
-    const val versionName = "5.4.1"
+    const val versionCode = 94
+    const val versionName = "5.4.2"
 
 
 }
