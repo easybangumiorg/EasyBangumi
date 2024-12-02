dc46ee1de689adff378f472a145c6bd583bbb0b9
heyanle <eke_le@163.com>
2024-12-03 01:32:01 +0800

    [feat] room 基建

--- room/src/commonMain/kotlin/com/heyanle/easy_bangumi_cm/room/Module.kt
+++ room/src/commonMain/kotlin/com/heyanle/easy_bangumi_cm/room/Module.kt
@@ -1,7 +1,10 @@
 package com.heyanle.easy_bangumi_cm.room
 
+import com.heyanle.easy_bangumi_cm.room.media.repository.MediaDatabase
+import com.heyanle.easy_bangumi_cm.room.media.repository.dao.MediaInfoDao
 import com.heyanle.easy_bangumi_cm.room.media.repository.mediaDatabaseModule
 import org.koin.core.module.Module
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 
@@ -12,4 +15,9 @@ import org.koin.dsl.module
 val roomModule: Module
     get() = module {
         includes(mediaDatabaseModule)
+
+        single {
+            val mediaDatabase: MediaDatabase = get()
+            mediaDatabase.mediaInfoDao()
+        } bind MediaInfoDao::class
     }
