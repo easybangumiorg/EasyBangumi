564418c5ff907abfc63d311b3df11079255f26cf
heyanle <eke_le@163.com>
2025-02-04 16:30:39 +0800

    [feat] plugin 模块 60%

--- common/plugin/core/src/main/kotlin/com/heyanle/easy_bangumi_cm/common/plugin/core/inner/InnerComponentBundle.kt
+++ common/plugin/core/src/main/kotlin/com/heyanle/easy_bangumi_cm/common/plugin/core/inner/InnerComponentBundle.kt
@@ -1,8 +1,12 @@
 package com.heyanle.easy_bangumi_cm.common.plugin.core.inner
 
+import com.heyanle.easy_bangumi_cm.common.plugin.core.EasyPluginConfigProvider
 import com.heyanle.easy_bangumi_cm.plugin.api.component.Component
 import com.heyanle.easy_bangumi_cm.plugin.api.component.ComponentBundle
 import com.heyanle.easy_bangumi_cm.plugin.api.source.Source
+import com.heyanle.lib.inject.core.Inject
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.reflect.KClass
 
 /**
  * Created by heyanlin on 2025/1/29.
@@ -10,6 +14,11 @@ import com.heyanle.easy_bangumi_cm.plugin.api.source.Source
 class InnerComponentBundle(
     private val innerSource: InnerSource
 ): ComponentBundle {
+
+    private val configProvider: EasyPluginConfigProvider by Inject.injectLazy()
+    private val bundle: HashMap<KClass<*>, Any> = hashMapOf()
+    private val componentProxy:  HashMap<KClass<*>, Any> = hashMapOf()
+    private val init = AtomicBoolean(false)
 
     override fun getSource(): Source {
         return innerSource
@@ -19,7 +28,7 @@ class InnerComponentBundle(
         TODO("Not yet implemented")
     }
 
-    fun load() {
+    suspend fun load() {
 
     }
 }
---
+++ common/plugin/utils/src/commonMain/kotlin/com/heyanle/easy_bangumi_cm/plugin/utils/StringHelper.kt
@@ -0,0 +1,14 @@
+package com.heyanle.easy_bangumi_cm.plugin.utils
+
+/**
+ * Created by heyanlin on 2025/1/31.
+ */
+interface StringHelper {
+
+    fun toast(text: String)
+
+    fun moeSnackBar(text: String)
+
+    fun moeDialog(text: String)
+
+}
