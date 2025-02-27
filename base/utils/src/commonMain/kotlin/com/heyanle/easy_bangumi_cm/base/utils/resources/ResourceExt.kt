2f43da19992ddc7a052728234dc42b26d8b6d010
heyanle <eke_le@163.com>
2025-02-27 18:51:53 +0800

    [feat] image 组件 99%

--- base/utils/src/commonMain/kotlin/com/heyanle/easy_bangumi_cm/base/utils/resources/ResourceExt.kt
+++ base/utils/src/commonMain/kotlin/com/heyanle/easy_bangumi_cm/base/utils/resources/ResourceExt.kt
@@ -4,8 +4,8 @@ package com.heyanle.easy_bangumi_cm.base.utils.resources
  * 保持 Any 方便后续迭代拓展资源类型
  * Created by heyanlin on 2025/2/6.
  */
-// Label - String StringResource
-// Drawable - String DrawableResource ImageVector Bitmap
+// Text - String StringResource
+// Image - String ImageResource AssetResource
 typealias ResourceOr = Any
 
 fun ResourceOr.isString(): Boolean{
